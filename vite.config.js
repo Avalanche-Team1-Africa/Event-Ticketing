e8f25868f485368f726d2d2f11574b88a1945ff3
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-02-22 19:14:33 +0300

    Enhance Vite configuration by adding support for ESBuild options and defining __dirname and __filename

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +16,9 @@ export default defineConfig({
   optimizeDeps: {
     include: ['@chakra-ui/react', 'ethers'],
   },
+  esbuild: {
+    supported: {
+      'streaming-sse': false,
+    }
+  }
 });
